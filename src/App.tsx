aa2d2ba1c06ac78f4b722651e6db9c5834e02d61
fpezcara <fpezcara@gmail.com>
2025-10-03 02:57:31 -0300

    Add loading component and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { lazy, Suspense } from 'react';
 import './App.css';
 import { useQuery } from '@tanstack/react-query';
 import type { LatestArrivals, LatestArrivalsData } from './types';
 
-import Card from './components/Card';
 import Loading from './components/Card/Loading';
 
-function App() {
+const CardComponent = lazy(() => import('./components/Card'));
+
+const App = () => {
   const fetchLatestArrivals = async (): Promise<LatestArrivals> =>
     await fetch(
       'https://www.greatfrontend.com/api/projects/challenges/e-commerce/products?collection=latest'
@@ -13,7 +15,7 @@ function App() {
 
   fetchLatestArrivals().then((data) => data);
 
-  const { data, isError, isLoading } = useQuery<LatestArrivals, Error>({
+  const { data, isError } = useQuery<LatestArrivals, Error>({
     queryKey: ['latestArrivals'],
     queryFn: fetchLatestArrivals,
   });
@@ -23,40 +25,32 @@ function App() {
     console.error('There was an error fetching data.');
   }
 
-  console.log('query', data?.data);
-  // console.log("is it loading?", isLoading);
-  // console.log("isError", isError);
-
   return (
-    <main className='m-4 flex flex-col rounded-lg bg-white px-3 py-12 md:px-4 md:py-16 lg:p-24'>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <section className='items-between flex justify-between'>
-            <h2 className='text-2xl font-semibold tracking-wide'>
-              Latest Arrivals
-            </h2>
-            <a className='size-lg cursor-pointer rounded-sm border border-gray-200 px-4 py-2 text-sm text-neutral-900 shadow-sm/20 hover:bg-gray-100'>
-              View all
-            </a>
-          </section>
-          <section className='mt-8 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
-            {data?.data.map((item: LatestArrivalsData) => (
-              <Card
-                key={item.product_id}
-                name={item.name}
-                images={item.images}
-                colors={item.colors}
-                listPrice={item.inventory[0].list_price}
-                salePrice={item.inventory[0].sale_price}
-              />
-            ))}
-          </section>
-        </>
-      )}
+    <main className='m-4 flex h-screen flex-col rounded-lg bg-white px-3 py-12 md:px-4 md:py-16 lg:p-24'>
+      <section className='items-between flex justify-between'>
+        <h2 className='text-2xl font-semibold tracking-wide'>
+          Latest Arrivals
+        </h2>
+        <a className='size-lg cursor-pointer rounded-sm border border-gray-200 px-4 py-2 text-sm text-neutral-900 shadow-sm/20 hover:bg-gray-100'>
+          View all
+        </a>
+      </section>
+      <section className='mt-8 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
+        {data?.data.map((item: LatestArrivalsData) => (
+          <Suspense fallback={<Loading />}>
+            <CardComponent
+              key={item.product_id}
+              name={item.name}
+              images={item.images}
+              colors={item.colors}
+              listPrice={item.inventory[0].list_price}
+              salePrice={item.inventory[0].sale_price}
+            />
+          </Suspense>
+        ))}
+      </section>
     </main>
   );
-}
+};
 
 export default App;
--- src/components/Card/Loading.tsx
+++ src/components/Card/Loading.tsx
@@ -1,5 +1,30 @@
 const Loading = () => {
-  return <h3>Loading.....</h3>;
+  return (
+    <div
+      role='status'
+      className='mx-auto w-full max-w-sm rounded-md border border-gray-300 p-4'
+    >
+      <div className='flex animate-pulse flex-col space-x-4'>
+        <svg
+          className='h-[300px] w-full text-gray-200 dark:text-gray-500'
+          aria-hidden='true'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='currentColor'
+          viewBox='0 0 20 18'
+        >
+          <path d='M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z' />
+        </svg>
+        <div className='flex flex-col gap-4'>
+          <div className='space-y-2'>
+            <div className='h-2 w-1/6 rounded bg-gray-300' />
+            <div className='h-2 w-3/6 rounded bg-gray-300' />
+          </div>
+          <div className='h-2 w-2/6 rounded bg-gray-300' />
+          <div className='h-5 w-5 rounded-full bg-gray-300' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
